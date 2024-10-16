dd15d4a644365676a9aa9cda93834afedf64b408
glennmandagi16 <s21710491@student.unklab.ac.id>
2024-10-17 04:23:15 +0800

    Update files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple JS to display a message when the page loads
+window.onload = function(){
+    alert("Welcome to my GitHub Page!");
+}
+
+function showMessage(){
+    alert("Button clicked");
+}
